32b5fef2b993a06c8a911c51cde164e24e3e2d82
Taisei <taisei96.web@gmail.com>
2023-06-03 19:19:00 +0900

    headerスタイル適用

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import "./App.css";
+import "./css/reset.css";
+import "./css/style.css";
 import Header from "./components/Header";
 
 const App = () => {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-      <header>
-        <div className='logo'>
-          <h3>ポートフォリオ</h3>
+      <header className='header'>
+        <div className='header-inner'>
+          <h1 className='logo'></h1>
+          <nav>
+            <ul className="nav-list">
+              <li><a href="#about">私について</a></li>
+              <li><a href="#service">サービス</a></li>
+              <li><a href="#works">制作実績</a></li>
+              <li><a href="#contact">お問い合わせ</a></li>
+            </ul>
+          </nav>
         </div>
       </header>
-
-      <nav>
-        <ul>
-          <li><a href="#">ホーム</a></li>
-          <li><a href="#">ブログ</a></li>
-          <li><a href="#">SNS</a></li>
-        </ul>
-      </nav>
     </>
   )
 }
