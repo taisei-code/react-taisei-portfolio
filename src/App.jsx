310100c19f7dd76d4ea4eac212a6b1dcb13328da
Taisei <taisei96.web@gmail.com>
2023-06-04 08:50:29 +0900

    ルーティング設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import "./css/reset.css";
 import "./css/style.css";
 import Header from "./components/Header";
+import HomePage from "./components/HomePage";
+import BlogPage from "./components/BlogPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/blog" element={<BlogPage />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
