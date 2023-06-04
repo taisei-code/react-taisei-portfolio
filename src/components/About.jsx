e51a78e63d1619a168d0e2f05cc04603e90d7e1d
Taisei <taisei96.web@gmail.com>
2023-06-04 17:34:47 +0900

    aboutコンポーネント雛形

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+       <section id="about" className="about fade-in">
+        <div className="inner">
+          <h2 className="section-title"><span>私について</span>ABOUT</h2>
+          <div className="about-wrapper">
+            <div className="about-left">
+              <img src="images/about-taisei.jpg" alt="" />
+            </div>
+            <div className="about-right">
+              <h3 className="about-title">シンプルで分りやすく、心を動かす。</h3>
+              <p className="about-lead">
+                はじめまして！埼玉県在住のTAISEIと申します。<br /><br />
+                【経歴】<br />
+                大学卒業後に飲食店フリーターを経て、現在は、都内の制作会社でWebエンジニアをしています。制作会社で得られたノウハウを活かすことはもちろんのこと、個人事業主として経営目線を持つことで、お客様に寄り添い、お客様の課題に正面から向き合ってまいります。
+                <br /><br />
+                【スキル一覧】<br />
+                HTML | CSS | JavaScript（jQuery）<br /><br />
+                【趣味】<br />
+                筋トレ | サウナ | カフェ巡り
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className="mv">
-      <div className="mv-wrapper">
-        <p className="mv-text">I'm TAISEI</p>
-        <a href="#contact" className="mv-btn">お問い合わせ</a>
+    <>
+      <div className="mv">
+        <div className="mv-wrapper">
+          <p className="mv-text">I'm TAISEI</p>
+          <a href="#contact" className="mv-btn">お問い合わせ</a>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
