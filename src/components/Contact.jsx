cc31a476882c5740ef4e71bebe95219ae4088032
Taisei <taisei96.web@gmail.com>
2023-06-05 13:36:58 +0900

    contactコンポーネント雛形

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,39 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+      <section id="contact" className="contact title-position_right fade-in">
+        <div className="inner">
+          <h2 className="section-title secContact-title"><span>お問い合わせ</span>CONTACT</h2>
+          <p className="contact-lead">
+            お仕事のご依頼やお問い合わせは下記フォームから受け付けています。<br className="pc_only" />
+            24時間以内に回答いたしますので、些細なことでもお気軽にご相談ください。
+          </p>
+          <form action="https://docs.google.com/forms/u/2/d/e/1FAIpQLSdQ9YAK2Zosa1QJ8KRmfdomHZff975uhwHMVGjsILCeNIn0Ig/formResponse" id="form">
+            <div className="item">
+              <label className="label"  for="name">お名前</label>
+              <input className="inputs" id="name" type="text" name="entry.280387875"required />
+            </div>
+            
+            <div className="item">  
+              <label className="label"  for="email">メールアドレス</label>
+              <input className="inputs" id="email" type="email" name="entry.945328814"required />
+            </div>
+            
+            <div className="item">
+              <label className="label" for="comment">メッセージ本文</label>
+              <textarea className="inputs" id="comment" name="entry.1921610804"required></textarea>
+            </div>
+
+            <div className="btn-area">
+              <input type="submit" className="submit_btn" value="送信する" />
+            </div>
+          </form>
+          {/* <p className="end_message">お問い合わせありがとうございました</p>
+          <p className="false_message">送信失敗です</p> */}
+        </div>
+      </section>
+    </>
   )
 }
 
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <section id="works" className="works fade-in">
+        <div className="inner">
+          <h2 className="section-title"><span>制作実績</span>WORKS</h2>
+          <div className="work-items">
+            <div className="work-item">
+              <a href="" target="_blank">
+                <div className="zoom-in-img">
+                  <img src="images/salad-cafe.jpg" className="work-lp-img" alt="" />
+                </div>
+                <div className="work-sentence-wrapper">
+                  <h3 className="work-title">Garden Salad</h3>
+                  <p className="work-head"> デザインカンプからコーディング（Figma）</p>
+                  <p className="work-detail">
+                    HTML、CSS、jQueryで静的な架空のLPを制作しました。
+                    PC、タブレット、スマートフォンなど、異なる画面サイズの
+                    幅を基準にWebサイト表示を柔軟に調整しています。
+                  </p>
+                  <p className="work-period">制作期間目安：10日</p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
