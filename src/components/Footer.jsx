f40ee272fd151a832206362f2cfeb26faba21c44
Taisei <taisei96.web@gmail.com>
2023-06-05 13:42:24 +0900

    Footerコンポーネント雛形

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer class="footer">
+        <div class="footer-bottom">
+          <small>Copyright &copy; 2022 TAISEI</small>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
