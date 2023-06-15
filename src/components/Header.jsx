4cb57c4b6119a044abd6c476ed39c086c6086662
Taisei <taisei96.web@gmail.com>
2023-06-15 09:39:54 +0900

    ハンバーガーメニュー実装

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,18 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import Navigation from './Navigation';
 import "../css/style.css";
-import ToggleButton from './ToggleButton';
+
 
 const Header = () => {
+  
   return (
     <>
       <header className="header">
-
         <div className="header-inner">
 
           <h1 className="logo"></h1>
-          <nav>
-            <ul className="nav-list">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/service">Service</Link>
-              </li>
-              <li>
-                <Link to="/works">Works</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
 
-          </nav>
-
-          <ToggleButton />
+          <Navigation />
 
         </div>
       </header>
